f2a7ae1df3ebe0c0bf55454417db052a6845742e
Wil Hall <wil@wilhall.com>
2013-07-28 21:25:05 -0400

    Added API Manager class and configuration

---
+++ api_manager.rb
@@ -0,0 +1,13 @@
+require 'jsonapi'
+require 'config.rb'
+
+class ApiManager
+
+  def initialize
+    @api = JSONAPI::JSONAPI.new(apiSettings)
+  end
+
+  def call(method, *args)
+    @api.call_api(method, *args)
+  end
+end
